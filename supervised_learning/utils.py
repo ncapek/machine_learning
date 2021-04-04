f8b72f5daca5eff5f1cd30baf2bcdefebaf15fd9
Nicholas Čapek <capekn@centrum.cz>
2021-04-04 13:03:23 +0200

    update to supervised learning

--- supervised_learning/utils.py
+++ supervised_learning/utils.py
@@ -35,3 +35,30 @@ def get_alternating_grid(size=8):
             t = (t + 1) % 2
         start_t = (start_t + 1) % 2
     return X, Y
+
+def get_modified_xor():
+    X = np.zeros((200, 2))
+    X[:50] = np.random.random((50, 2)) / 2 + 0.5
+    X[50:100] = np.random.random((50, 2)) / 2
+    X[100:150] = np.random.random((50, 2)) / 2 + np.array([0, 0.5])
+    X[150:] = np.random.random((50, 2)) / 2 + np.array([0.5, 0])
+    Y = np.array([0]*100 + [1]*100)
+    return X, Y
+
+def get_donut():
+    N = 200
+    R_inner = 5
+    R_outer = 10
+
+    R1 = np.random.randn(int(N/2)) + R_inner
+    theta = 2*np.pi*np.random.random(int(N/2))
+    X_inner = np.concatenate([[R1*np.cos(theta)], [R1*np.sin(theta)]]).T
+
+    R2 = np.random.randn(int(N/2)) + R_outer
+    theta = 2 * np.pi*np.random.random(int(N/2))
+    X_outer = np.concatenate([[R2 * np.cos(theta)], [R2 * np.sin(theta)]]).T
+
+    X = np.concatenate([X_inner, X_outer])
+    Y = np.array([0]*int(N/2) + [1]*int(N/2))
+
+    return X, Y
